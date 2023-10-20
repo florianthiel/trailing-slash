4befd85a2868a61b7dbfebf7a59ed1d69bab05ab
florianthiel <thiel.florian@me.com>
2023-10-20 09:57:50 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  routeRules: {
+    '/isr/': {
+      isr: true
+    },
+    '/prerender/': {
+      prerender: true
+    },
+    '/ssr/': {
+      ssr: true
+    },
+    '/csr/': {
+      ssr: false
+    }
+  }
+})
